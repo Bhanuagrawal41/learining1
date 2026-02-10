3e98c094c07a4712f9a34de8d1172d8ea18fe0d3
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-02-10 21:52:25 +0530

    another one

--- javascript/new.js
+++ javascript/new.js
@@ -1,1 +1,46 @@
-console.log("hello world")
+// let pencilprice = 10;
+// let eraserprice = 29;
+// console.log( `hello ${pencilprice + eraserprice} hy how are you`);
+
+// let age = 18;
+// console.log(ag
+// let color = "red";
+
+
+
+//  if(color == "red"){
+//     console.log("wait sir");
+//  }
+//  if(color == "yellow"){
+//     console.log("slow down man");
+
+//  }
+//  if(color == "green"){
+//     console.log("ready to go yo");
+//  }
+// let age = 5;
+// if(age => 18){
+//     console.log("you can vote");
+// }
+// else if(age < 18){
+//     console.log('you are not eligible');
+// }
+
+let size = 'e';
+
+if(size === 'xl'){
+    console.log("price is 250 ruppe")
+}
+else if(size === "l"){
+    console.log('price is 200');
+
+}
+ else if(size === "M"){
+    console.log("price is 100");
+ }
+ else if(size === "s"){
+    console.log("price is 50");
+ }
+ else{
+    console.log("invalid ammoount");
+ }
