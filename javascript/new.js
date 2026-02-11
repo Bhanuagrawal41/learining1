2ecfffd18ba3415e2c078f0450f6335ef09e5dd5
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-02-11 20:41:19 +0530

    day8

--- javascript/new.js
+++ javascript/new.js
@@ -26,21 +26,71 @@
 //     console.log('you are not eligible');
 // }
 
-let size = 'e';
+// let size = 'e';
 
-if(size === 'xl'){
-    console.log("price is 250 ruppe")
-}
-else if(size === "l"){
-    console.log('price is 200');
+// if(size === 'xl'){
+//     console.log("price is 250 ruppe")
+// }
+// else if(size === "l"){
+//     console.log('price is 200');
 
+// }
+//  else if(size === "M"){
+//     console.log("price is 100");
+//  }
+//  else if(size === "s"){
+//     console.log("price is 50");
+//  }
+//  else{
+//     console.log("invalid ammoount");
+//  }
+
+//  let ring ="all";
+// if(ring[0] === "a" || ring.length >= 3 ){
+//    console.log("its a good string");
+// }
+
+
+
+// let color = "red";
+
+// switch (color){
+//    case "red":
+//       console.log("stop");
+//       break
+//    case "green":
+//       console.log("green");
+//    case "yellow":
+//       console.log("wait");
+//    default:
+//       console.log("light is broken");
+// }
+
+let num = 2;
+
+switch (num){
+   case 1:
+      console.log("monday");
+      break;
+   case 2:
+   console.log("tuesday" );
+   break;
+
+   case 3:
+      console.log("wednesday");
+      break;
+   case 4:
+      console.log("thursday");
+      break;
+
+   case 5:
+      console.log("friday");
+      break;
+   case 6:
+      console.log("saterday");
+      break;
+
+   case 7:
+      console.log("sunday");
+      break;
 }
- else if(size === "M"){
-    console.log("price is 100");
- }
- else if(size === "s"){
-    console.log("price is 50");
- }
- else{
-    console.log("invalid ammoount");
- }
