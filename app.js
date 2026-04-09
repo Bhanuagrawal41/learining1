6d91f2ea43fe4d4004fa697f370edcf952714347
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-04-09 20:11:20 +0530

    starting again

---
+++ app.js
@@ -0,0 +1,13 @@
+// let student ={
+//     name : "bhanu blinders",
+// }
+// creating the object literal object literal having the key and value pair name bhanu agrawal name is key and value is bahnu blindeer
+
+/// createing a thread or twidter post 
+const post = {
+    username : "bhanu agrawal",
+    content : "funny topic",
+    likes: 1000,
+    tags: ["apna college", "best friend"]
+
+}
--- javascript/new.js
+++ javascript/new.js
@@ -182,82 +182,109 @@
 
 
 // Creating an array
-let arr = [10, 20, 30, 40, 50];
+// let arr = [10, 20, 30, 40, 50];
 
-console.log("Original Array:", arr);
+// console.log("Original Array:", arr);
 
-// 1. Add elements
-arr.push(60);        // add at end
-arr.unshift(5);      // add at beginning
-console.log("After push & unshift:", arr);
+// // 1. Add elements
+// arr.push(60);        // add at end
+// arr.unshift(5);      // add at beginning
+// console.log("After push & unshift:", arr);
 
-// 2. Remove elements
-arr.pop();           // remove last
-arr.shift();         // remove first
-console.log("After pop & shift:", arr);
+// // 2. Remove elements
+// arr.pop();           // remove last
+// arr.shift();         // remove first
+// console.log("After pop & shift:", arr);
 
-// 3. Length of array
-console.log("Length:", arr.length);
+// // 3. Length of array
+// console.log("Length:", arr.length);
 
-// 4. Access elements
-console.log("First element:", arr[0]);
-console.log("Last element:", arr[arr.length - 1]);
+// // 4. Access elements
+// console.log("First element:", arr[0]);
+// console.log("Last element:", arr[arr.length - 1]);
 
-// 5. Loop through array
-console.log("Using for loop:");
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+// // 5. Loop through array
+// console.log("Using for loop:");
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// // 6. forEach method
+// console.log("Using forEach:");
+// arr.forEach(function(value, index) {
+//     console.log(index, value);
+// });
+
+// // 7. Map (create new array)
+// let doubled = arr.map(num => num * 2);
+// console.log("Doubled array:", doubled);
+
+// // 8. Filter (condition)
+// let greaterThan25 = arr.filter(num => num > 25);
+// console.log("Greater than 25:", greaterThan25);
+
+// // 9. Reduce (sum of elements)
+// let sum = arr.reduce((total, num) => total + num, 0);
+// console.log("Sum:", sum);
+
+// // 10. Find element
+// let found = arr.find(num => num > 25);
+// console.log("First number > 25:", found);
+
+// // 11. Includes
+// console.log("Contains 30?", arr.includes(30));
+
+// // 12. IndexOf
+// console.log("Index of 40:", arr.indexOf(40));
+
+// // 13. Slice (copy part of array)
+// let part = arr.slice(1, 4);
+// console.log("Slice(1,4):", part);
+
+// // 14. Splice (add/remove in middle)
+// arr.splice(2, 1, 99);  // remove 1 element at index 2 and add 99
+// console.log("After splice:", arr);
+
+// // 15. Reverse & Sort
+// let nums = [5, 2, 9, 1, 7];
+
+// nums.sort((a, b) => a - b);   // ascending sort
+// console.log("Sorted:", nums);
+
+// nums.reverse();
+// console.log("Reversed:", nums);
+
+// // 16. Merge arrays
+// let arr2 = [100, 200];
+// let merged = arr.concat(arr2);
+// console.log("Merged:", merged);
+
+// // 17. Convert array to string
+// console.log("Join:", arr.join("-"));
+
+
+// let favmovie = "dark";
+// let movie = prompt("enter your favourite movie");
+// while((movie != favmovie) && (movie != "quit")){
+//     favmovie = prompt("wrong guess try again");
+
+// }
+
+// if (movie == favmovie){
+//     console.log("you are correct");
+
+// } else {
+//     console.log("you quit the game");
+// }
+
+
+let i = 1;
+while(i <= 5){
+
+     if(i==4){
+        break;
+     }
+    console.log(i);
+    i++;
+
 }
-
-// 6. forEach method
-console.log("Using forEach:");
-arr.forEach(function(value, index) {
-    console.log(index, value);
-});
-
-// 7. Map (create new array)
-let doubled = arr.map(num => num * 2);
-console.log("Doubled array:", doubled);
-
-// 8. Filter (condition)
-let greaterThan25 = arr.filter(num => num > 25);
-console.log("Greater than 25:", greaterThan25);
-
-// 9. Reduce (sum of elements)
-let sum = arr.reduce((total, num) => total + num, 0);
-console.log("Sum:", sum);
-
-// 10. Find element
-let found = arr.find(num => num > 25);
-console.log("First number > 25:", found);
-
-// 11. Includes
-console.log("Contains 30?", arr.includes(30));
-
-// 12. IndexOf
-console.log("Index of 40:", arr.indexOf(40));
-
-// 13. Slice (copy part of array)
-let part = arr.slice(1, 4);
-console.log("Slice(1,4):", part);
-
-// 14. Splice (add/remove in middle)
-arr.splice(2, 1, 99);  // remove 1 element at index 2 and add 99
-console.log("After splice:", arr);
-
-// 15. Reverse & Sort
-let nums = [5, 2, 9, 1, 7];
-
-nums.sort((a, b) => a - b);   // ascending sort
-console.log("Sorted:", nums);
-
-nums.reverse();
-console.log("Reversed:", nums);
-
-// 16. Merge arrays
-let arr2 = [100, 200];
-let merged = arr.concat(arr2);
-console.log("Merged:", merged);
-
-// 17. Convert array to string
-console.log("Join:", arr.join("-"));
