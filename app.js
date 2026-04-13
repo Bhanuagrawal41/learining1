726a4a389cf2c6d71b01283b7d186c4009cb4b18
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-04-13 22:54:16 +0530

    hello i am back

--- app.js
+++ app.js
@@ -4,10 +4,40 @@
 // creating the object literal object literal having the key and value pair name bhanu agrawal name is key and value is bahnu blindeer
 
 /// createing a thread or twidter post 
-const post = {
-    username : "bhanu agrawal",
-    content : "funny topic",
-    likes: 1000,
-    tags: ["apna college", "best friend"]
+// const post = {
+//     username : "bhanu agrawal",
+//     content : "funny topic",
+//     likes: 1000,
+//     tags: ["apna college", "best friend"]
 
-}
+// }
+
+
+
+//--------------what a object literal code is in java script--------------//
+// const name = {
+//      aman : {
+//         name : "aman",
+//         age : 32,
+//         class : "first class"
+//      },
+//      shradha : {
+//         name : "sharadha mam",
+//         age :  30
+
+//      }
+
+// }
+//---------------------------///
+
+
+//now learning the math random thing in javascript
+// console.log(Math.random()) // it will give us the random number between 0 and 1
+// console.log(Math.random()*10) // it will give us the random number between 0 and 10
+// console.log(Math.floor(Math.random()*10)) // it will give us the random number between 0 and 9 because of floor function it will round down the number to the nearest integer
+
+
+//------------------creating a random number between 1 and 100------------------//
+// console.log(Math.floor(Math.random()*100)+1) // it will give us the random number between 1 and 100 because of floor function it will round down the number to the nearest integer and we are adding 1 to make it between 1 and 100       
+
+prompt  = ("enter the max no");
