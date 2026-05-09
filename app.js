89ca171d5b57cf2a3f1a7a1f327bc846fa2a48eb
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-05-09 06:18:07 +0530

    resumed

--- app.js
+++ app.js
@@ -40,4 +40,53 @@
 //------------------creating a random number between 1 and 100------------------//
 // console.log(Math.floor(Math.random()*100)+1) // it will give us the random number between 1 and 100 because of floor function it will round down the number to the nearest integer and we are adding 1 to make it between 1 and 100       
 
-prompt  = ("enter the max no");
+
+
+
+// function diceroll(){
+//    let rand =  (Math.random() *6) + 1;
+//    console.log(Math.floor(rand))  ;  
+// }
+
+// diceroll();
+
+
+// function avg(a,b,c){
+//     return (a+b+c)/3;
+// }
+
+
+// function table(n){
+//     for(let i = 1; i <=10; i++){
+//         let ans = n * i;
+//         console.log(ans);
+//     }
+
+    
+// }
+
+// console.log(table(4));
+
+// function sum(n){
+//     let sum = 0;
+//     for(let i = 1; i <= n; i++){
+//          sum = sum + i;
+
+//     }
+//     return sum;
+// }
+// //  console.log(sum(6));
+// for(let i =1; i<=5; i++){
+//     console.log(i);
+// }
+
+
+function outerFunc(){
+    let x = 5;
+    let y = 6;
+    function innerfunc(){
+        console.log(x+y);
+    }
+    innerfunc();
+}
+outerFunc();
