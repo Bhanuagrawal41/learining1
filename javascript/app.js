04b2880c32138a17773371c37ceb6711d2553466
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-16 17:23:25 +0530

    hello id

---
+++ javascript/app.js
@@ -0,0 +1,15 @@
+// let h1 = document.querySelector("h1");
+// // h1.textContent = "hello rishika kaise ho";
+// h1.innerHTML = "<i>hey i love yu rishiiiee</>";
+// // h1.hidden = true;
+// console.dir(h1);
+
+// let a = document.querySelector("a");
+// consol.log(a.getAttribute("href"));
+// // a.setAttribute("href", "https://www.instagram.com/riiisheee/");
+// // a.href = "https://www.instagram.com/riiisheee/"
+// a.removeAttribute("href");
+
+let h1 = document.createElement("h1");
+h1.textContent = "hey rishika kaise ho"; 
+document.body.append(h1);
