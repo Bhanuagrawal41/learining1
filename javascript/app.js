f70acef7ecc687a1682a4cfdb81b9f4300b8d335
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-16 17:39:25 +0530

    revising the js

--- javascript/app.js
+++ javascript/app.js
@@ -10,6 +10,14 @@
 // // a.href = "https://www.instagram.com/riiisheee/"
 // a.removeAttribute("href");
 
-let h1 = document.createElement("h1");
-h1.textContent = "hey rishika kaise ho"; 
-document.body.append(h1);
+// let h1 = document.createElement("h1");
+// h1.textContent = "ok as you wish my sweetheart"; 
+// document.body.prepend(h1);
+// let h2 = document.createElement("h2");
+// h2.textContent = "me bhi acchi hu i was saying lets meet on 24 aug";
+// document.querySelector("body").prepend(h2);
+//js se css bdalna
+
+let h1 = document.querySelector("h1");
+h1.style.color = "pink";
+h1.style.fontFamily = "cursive";
