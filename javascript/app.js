d98624a498a3ffae98b21afda065f07887624080
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-18 18:08:23 +0530

    :

--- javascript/app.js
+++ javascript/app.js
@@ -1,20 +1,20 @@
 // let h1 = document.querySelector("h1");
 // // h1.textContent = "hello rishika kaise ho";
-// h1.innerHTML = "<i>hey i love yu rishiiiee</>";
+// h1.innerHTML = "<i>hey hy ribba</>";
 // // h1.hidden = true;
 // console.dir(h1);
 
 // let a = document.querySelector("a");
 // consol.log(a.getAttribute("href"));
-// // a.setAttribute("href", "https://www.instagram.com/riiisheee/");
-// // a.href = "https://www.instagram.com/riiisheee/"
+// // a.setAttribute("href",");
+// // a.href = "
 // a.removeAttribute("href");
 
 // let h1 = document.createElement("h1");
-// h1.textContent = "ok as you wish my sweetheart"; 
+// h1.textContent = "ok as you wish "; 
 // document.body.prepend(h1);
 // let h2 = document.createElement("h2");
-// h2.textContent = "me bhi acchi hu i was saying lets meet on 24 aug";
+// h2.textContent = "me bhi";
 // document.querySelector("body").prepend(h2);
 //js se css bdalna
 
