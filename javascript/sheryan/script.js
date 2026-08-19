ff7ddef52dadef246957684309a00f3c689c7115
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-19 22:00:19 +0530

    learned something new today :)

---
+++ javascript/sheryan/script.js
@@ -0,0 +1,34 @@
+// let buynow = document.querySelectorAll(".buy-now");
+// console.log(buynow);
+
+// let mykanne = document.querySelector("#mykanne");
+// mykanne.textContent = "welcome home rishhieen";
+
+// let lis = document.querySelectorAll("li");
+
+// // lis.forEach(function(val){
+// //     console.log(val.textContent);
+// // });
+// for(let i = 0; i<lis.length; i++){
+//     console.log(lis[i].textContent);
+// }
+
+
+// let p  = document.querySelector("p");
+// p.innerHTML = "<b> updated bold </b> by javaScript";
+
+// let img = document.querySelector("img");
+// console.log(img.getAttribute("src"));
+// // console.log(img.src);
+
+// let img = document.querySelector("img").setAttribute("src","https://images.unsplash.com/photo-1786658054420-bbdcd866c2b9?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+
+// let a = document.querySelector("a");
+// a.href = "www.linkedinl.com"
+
+// let btn = document.querySelector("button");
+// btn.removeAttribute("disabled");
+
+// what does the createeleent () do what's returned ?
+
+let h1 = document.createElement("h1");
